c1b056f6418800197937a4f3a4c4786ed116bee2
hillsg02 <gareth.hillston@bbc.co.uk>
2026-08-18 13:38:07 +0100

    Remoe describe block

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,30 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Page interactions', () => {
-  test('has correct title', async ({ page }) => {
-    await page.goto('/');
-    await expect(page.getByRole('heading')).toHaveText('My Todos');
-  });
-  
-  // Has correct description
-
-  // beforeEach?
-
-  // Has free text box
-
-  // Has add button
-  
-  // Can add list item
-  
-  // Can add multiple list items
-
-  // Can remove list item
-
-
-  // Routing -- split into describe blocks?
-
-  //  Can route to About
-  //  Can route back to Home from About
-  //  Can route to Author
-  //  Can route to Test
+test('has correct title', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByRole('heading')).toHaveText('My Todos');
 });
+
+// Has correct description
+
+// beforeEach?
+
+// Has free text box
+
+// Has add button
+
+// Can add list item
+
+// Can add multiple list items
+
+// Can remove list item
+
+
+// Routing -- split into describe blocks?
+
+//  Can route to About
+//  Can route back to Home from About
+//  Can route to Author
+//  Can route to Test
+
+// Ya did it!
