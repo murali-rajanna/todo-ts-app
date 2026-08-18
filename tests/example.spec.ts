e82043761696bb98820d8f7eaf44fe1c1df51ad7
hillsg02 <gareth.hillston@bbc.co.uk>
2026-08-18 13:34:41 +0100

    Replaced tests with comment hints

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,96 +1,30 @@
-import { test, expect, devices } from '@playwright/test';
-
-
-test.beforeEach(async ({ page }) => {
-  await page.goto('/');
-});
+import { test, expect } from '@playwright/test';
 
 test.describe('Page interactions', () => {
   test('has correct title', async ({ page }) => {
+    await page.goto('/');
     await expect(page.getByRole('heading')).toHaveText('My Todos');
   });
   
-  test('has correct description', async ({ page }) => {
-    await expect(page.getByRole('paragraph')).toHaveText('Stay organised, one task at a time');
-  });
+  // Has correct description
 
-  test('has free text box', async ({ page }) => {
-    await expect(page.getByRole('textbox')).toBeVisible();
-    await expect(page.getByRole('textbox')).toHaveAttribute('placeholder');
-    await expect(page.getByPlaceholder('What needs to be done?')).toBeVisible();
-  });
+  // beforeEach?
 
-  test('has add button', async ({ page }) => {
-    await expect(page.getByRole('button')).toBeVisible();
-    await expect(page.getByRole('button')).toHaveText('Add');
-  });
+  // Has free text box
+
+  // Has add button
   
-  test('can add list item', async ({ page }) => {
-    await page.getByRole('textbox').fill('Hello');
-    await page.locator('#add-btn').click();
-    await expect(page.getByText('Hello')).toBeVisible();
-  });
+  // Can add list item
   
-  test('can add multiple list items', async ({ page }) => {
-    await page.getByRole('textbox').fill('Hello');
-    await page.locator('#add-btn').click();
-    await page.getByRole('textbox').fill('There');
-    await page.locator('#add-btn').click();
-    await page.getByRole('textbox').fill('General');
-    await page.locator('#add-btn').click();
-    await expect(page.getByText('Hello')).toBeVisible();
-    await expect(page.getByText('There')).toBeVisible();
-    await expect(page.getByText('General')).toBeVisible();
-  });
+  // Can add multiple list items
 
-  test('can remove list item', async ({ page }) => {
-    await page.getByRole('textbox').fill('Hello');
-    await page.locator('#add-btn').click();
-    await page.getByRole('textbox').fill('There');
-    await page.locator('#add-btn').click();
-    await page.getByRole('textbox').fill('General');
-    await page.locator('#add-btn').click();
-    await expect(page.getByText('Hello')).toBeVisible();
-    await expect(page.getByText('There')).toBeVisible();
-    await expect(page.getByText('General')).toBeVisible();
-    
-    await page.locator('#delete-btn-2').click();
-    await expect(page.getByText('Hello')).toBeVisible();
-    await expect(page.getByText('There')).not.toBeVisible();
-    await expect(page.getByText('General')).toBeVisible();
-  });
-});
+  // Can remove list item
 
-test.describe('Routing', () => {
-  test('can route to About', async ({ page }) => {
-    await page.getByTestId('nav-link-about').click();
-    
-    await expect(page).toHaveURL(/(.)+\/about/);
-    await expect(page.getByRole('heading')).toHaveText('About');
-  });
-  
-  test('can route back again', async ({ page }) => {
-    await page.getByTestId('nav-link-about').click();
-    
-    await expect(page).toHaveURL(/(.)+\/about/);
-    await expect(page.getByRole('heading')).toHaveText('About');
-    
-    await page.getByTestId('nav-link-todos').click();
-    await expect(page).toHaveURL(/(.)+\//);
-    await expect(page.getByRole('heading')).toHaveText('My Todos');
-  });
-  
-  test('can route to Author', async ({ page }) => {
-    await page.getByTestId('nav-link-author').click();
-    
-    await expect(page).toHaveURL(/(.)+\/author/);
-    await expect(page.getByRole('heading')).toHaveText('Author');
-  });
 
-  test('can route to Test', async ({ page }) => {
-    await page.getByTestId('nav-link-test').click();
-    
-    await expect(page).toHaveURL(/(.)+\/test/);
-    await expect(page.getByRole('heading')).toHaveText('Test');
-  });
+  // Routing -- split into describe blocks?
+
+  //  Can route to About
+  //  Can route back to Home from About
+  //  Can route to Author
+  //  Can route to Test
 });
