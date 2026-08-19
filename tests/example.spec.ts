6f4b9bf4935872057f27c7e3c0546a5c98f9a931
hillsg02 <gareth.hillston@bbc.co.uk>
2026-08-19 17:04:02 +0100

    Updated tasks for clarity

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -15,7 +15,7 @@ import { test, expect } from '@playwright/test';
  * Task 2:
  * We now have a couple tests routing to the homepage, and we can expect that the rest will as well.
  * It would be helpful to define the routing once for all tests.
- * Try using a beforeEach before your two tests to do the routing, and remove routing from the tests:
+ * Try adding a beforeEach before your two tests to do the routing, and remove routing from the tests:
  * 
 **/
 test.beforeEach(async ({ page }) => {
@@ -31,45 +31,46 @@ test.beforeEach(async ({ page }) => {
 
 /** 
  * Task 4:
- * Write a test to check that the add button is showing, with the correct text.
-**/
-
-/** 
- * We're about to write some tests for functionality, rather than checking page elements are present.
- * You might want to add a new describe block for these?
- * It will help divide up the tests in the playwright UI!
- * Make sure the beforeEach is outside the describe blocks, or it will only apply to the one it's inside.
+ * Write a test to check that the add button is showing, using the button role, with the correct text.
 **/
 
 /** 
  * Task 5:
+ * We're about to write some tests for functionality, rather than checking page elements are present.
+ * A new describe block will help group the tests, which will show up nicely in the UI.
+ * Make sure the beforeEach is outside the describe blocks, or it will only apply to the one it's inside.
+**/
+
+/** 
+ * Task 6:
  * Write a test to add a list item.
  * You'll need to find the text box, add some text with .fill(), click the button with .click(), then check that the new item is visible.
 **/
 
 /** 
- * Task 6:
+ * Task 7:
  * Write a test to add multiple list items.
- * You might have trouble selecting the add button, if you're selecting by role "button" - there's more than one now!
- * You can select an element by ID by using page.locator('#exampleid'), IDs should be unique.
+ * You might have trouble selecting the add button, if you're selecting by role "button" - the list items have buttons, too!
+ * You can select an element by ID by using page.locator('#exampleid'), IDs should always be unique.
 **/
 
 /** 
- * Task 7:
+ * Task 8:
  * Write a test to remove a list item.
  * First you need to build a list, as before.
- * Then, you'll need to select the delete button on the list item you want.
+ * Then, you'll need to select the delete button on the list item you want to remove.
  * Then, check that the item isn't there any more.
  * You can invert any assertion by putting .not in front, so for not visible it would be - .not.toBeVisible()
 **/
 
 /** 
+ * Task 9:
  * Now we want to check that the links at the top route to the different pages.
  * Another describe block would be helpful here.
 **/
 
 /** 
- * Task 9:
+ * Task 10:
  * Write a test to route to the About page, by clicking the tab at the top.
  * You'll need to check the url is correct afterwards, using .toHaveURL()
  * The parameter here isn't a string, it's a new language called Regex, so you can write .toHaveURL(/about/)
@@ -77,16 +78,16 @@ test.beforeEach(async ({ page }) => {
 **/
 
 /** 
- * Task 10:
+ * Task 11:
  * Write a test for navigating to the Todos page from another page.
- * You'll need to start on another page for this, and it doesn't make sense to go to home and then to that page.
- * So here, you'll need to move the beforeEach again. The two previous describes each get a copy, this block gets none.
+ * You'll need to start on another page for this, and it doesn't make sense to go to home and then to that page, so we no longer want the beforeEach to apply to every test.
+ * So here, you'll need to move the beforeEach again. The two other describe blocks each get a copy at the start, this block gets doesn't.
  * This means the first routing test needs a page.goto('/'), and this test will need page.goto('/about/')
  * To check that we navigate back to home we need do this check: .toHaveURL(/\//)
  * 
  * This is an example of Regex being weird, you don't need to worry about it, but if you care here's the explaination:
- * The slashes either end mark it as Regex.
- * \/ Means check for a forward slash, the back slash "escapes" the forward slash so that it doesn't count as a Regex marker, like the first and last one.
+ * The slashes at either end mark it as Regex.
+ * \/ means check for a forward slash. The back slash "escapes" the forward slash so that it doesn't count as a Regex marker.
 **/
 
 /* You did it! Have a cookie 🍪 */
